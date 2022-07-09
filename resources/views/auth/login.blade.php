7f5e69e2e404d89ebdb3b9d08759b814fcf0675d
Shawn North <shawnnorth@shawns-air.lan>
2022-07-08 20:19:22 -0700

    initial commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
             <span class="sr-only">{{ env('APP_NAME') }}</span>
             <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow">
         </a>
-        <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
+        <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account.</h2>
         <p class="mt-2 text-center text-gray-600">
         Or
         <a href="{{ route('register') }}" class="font-medium text-indigo-600 hover:text-indigo-500"> start your 7-day free trial </a>
